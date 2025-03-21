75c1399b2aafbc35bd4310c51ec82b2cbc4d9cee
Maria Eduarda Marques <Dudasme2445@hotmail.com>
2025-03-20 21:08:15 -0300

    Add eventos duração

--- ControleDePresenca/Models/Evento.cs
+++ ControleDePresenca/Models/Evento.cs
@@ -5,6 +5,8 @@
         public int EventoId { get; set; }
         public int EventoNome { get; set; }
 
+        public int Duracao { get; set; }
+
         public ICollection <Participante> Participantes { get; set; }
     }
 }
