b0034f43fcca0a154644047d5dee6c4f14ba8e5a
talles souza <tallesg765@gmail.com>
2025-03-20 21:16:43 -0300

    Configurações do banco de dados

--- ControleDePresenca/Models/Context.cs
+++ ControleDePresenca/Models/Context.cs
@@ -1,7 +1,23 @@
-﻿namespace ControleDePresenca.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using ControleDePresenca.Models;
+
+
+namespace ControleDePresenca.Models
 {
-    public class Context
+    public class Context : DbContext
     {
-      
+        public Context(DbContextOptions<Context> options)
+              : base(options)
+        {
+
+        }
+        public DbSet<Evento> Eventos { get; set; }//dbset é tabela
+        public DbSet<Participante> Participantes { get; set; }//tabela
     }
 }
--- ControleDePresenca/Program.cs
+++ ControleDePresenca/Program.cs
@@ -1,7 +1,28 @@
+using ControleDePresenca.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseDefaultServiceProvider(options =>
+{
+    options.ValidateScopes = false; // Ativa a validação de escopo. False evita erro de BD inexistente
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configuração da Entity Framework Core
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration["Data:Exemplo_EF_BD25:ConnectionString"],
+
+    //evita que o BD não seja criado por problemas de timeout com o servidor
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 10,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null);
+    }));
 
 var app = builder.Build();
 
@@ -23,5 +44,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//SeedData.Initialize(app.Services);
+SeedData.EnsurePopulated(app);
 
 app.Run();
