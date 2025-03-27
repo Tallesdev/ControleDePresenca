6edb969b3c232ab0d980adb1227d205ea09a467a
Maria Eduarda Marques <Dudasme2445@hotmail.com>
2025-03-27 15:07:08 -0300

    Customização do login e Registro do index java e css

--- ControleDePresenca/wwwroot/js/script.js
+++ ControleDePresenca/wwwroot/js/script.js
@@ -1,11 +1,11 @@
-﻿const conteiner = document.querySelector('.conteiner');
+﻿const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
 
 registerBtn.addEventListener('click', () => {
-    conteiner.classList.add('active'); // muda o conteiner da div para ativo assim fazendo a animacao
+    container.classList.add('active'); // muda o conteiner da div para ativo assim fazendo a animacao
 });
 
 loginBtn.addEventListener('click', () => {
-    conteiner.classList.remove('active'); // remove o ativo voltando o conteiner para login
+    container.classList.remove('active'); // remove o ativo voltando o conteiner para login
 });
